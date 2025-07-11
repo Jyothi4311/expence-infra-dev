3065db31aa19cf5a6890495ea518333fd912ce8b
Jyothi <jyothiveeranala3@gmail.com>
2025-07-11 18:10:10 +0530

    expense-infra

--- 00-vpc/outputs.tf
+++ 00-vpc/outputs.tf
@@ -5,3 +5,7 @@
 # output "subnets_info" {
 #     value = module.vpc.subnets_info
 # }
+
+output "public_subnet_ids" {
+    value = module.vpc.public_subnet_ids
+}
--- 10-sg/provider.tf
+++ 10-sg/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket = "jyothi-remote-dev"
-    key    = "expense-dev-vpc" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
+    key    = "expense-dev-sg" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
     region = "us-east-1"
     dynamodb_table = "jyothi-state-locking-dev"
   }
